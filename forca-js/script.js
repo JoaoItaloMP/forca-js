460a9fdfc251b43b94075b0892c5864e746c23ce
JoaoItaloMP <mastedhd@gmail.com>
2026-06-16 23:26:10 -0300

    feat: adicionar sistema de tentativas

--- forca-js/script.js
+++ forca-js/script.js
@@ -3,18 +3,22 @@ const palavra = "PROGRAMACAO";
 const palavraDiv = document.getElementById("palavra");
 const input = document.getElementById("letra");
 const botao = document.getElementById("btnEnviar");
+const mensagem = document.getElementById("mensagem");
+const tentativasTexto = document.getElementById("tentativas");
 
 let letrasCorretas = [];
+let letrasErradas = [];
+let tentativas = 6;
 
 function atualizarPalavra() {
 
     let exibicao = "";
 
-    for(let letra of palavra){
+    for (let letra of palavra) {
 
-        if(letrasCorretas.includes(letra)){
+        if (letrasCorretas.includes(letra)) {
             exibicao += letra + " ";
-        }else{
+        } else {
             exibicao += "_ ";
         }
 
@@ -27,13 +31,32 @@ botao.addEventListener("click", () => {
 
     const tentativa = input.value.toUpperCase();
 
-    if(
-        tentativa &&
-        palavra.includes(tentativa) &&
-        !letrasCorretas.includes(tentativa)
-    ){
-        letrasCorretas.push(tentativa);
+    if (!tentativa) {
+        return;
     }
+
+    if (palavra.includes(tentativa)) {
+
+        if (!letrasCorretas.includes(tentativa)) {
+            letrasCorretas.push(tentativa);
+        }
+
+        mensagem.textContent = "Letra correta!";
+
+    } else {
+
+        if (!letrasErradas.includes(tentativa)) {
+
+            letrasErradas.push(tentativa);
+            tentativas--;
+
+        }
+
+        mensagem.textContent = "Letra incorreta!";
+    }
+
+    tentativasTexto.textContent =
+        `Tentativas restantes: ${tentativas}`;
 
     atualizarPalavra();
 
