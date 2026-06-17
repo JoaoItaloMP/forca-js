73a1b0a2ad800973bd8d9543d0b9f2f8d844b58a
JoaoItaloMP <mastedhd@gmail.com>
2026-06-16 23:18:15 -0300

    feat: implementar exibição da palavra secreta

--- forca-js/script.js
+++ forca-js/script.js
@@ -1,1 +1,11 @@
-console.log("Jogo iniciado");
+const palavra = "PROGRAMACAO";
+
+const palavraDiv = document.getElementById("palavra");
+
+let exibicao = "";
+
+for(let i = 0; i < palavra.length; i++){
+    exibicao += "_ ";
+}
+
+palavraDiv.textContent = exibicao;
